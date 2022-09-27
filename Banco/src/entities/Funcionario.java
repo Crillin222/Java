b40f53930f42c5f5e7fdfe542a7788350484efda
Crillin222 <iuricasagrande@gmail.com>
2022-09-27 19:14:58 -0300

    adicionado projeto banco e suas entidades

---
+++ Banco/src/entities/Funcionario.java
@@ -0,0 +1,34 @@
+package entities;
+
+public class Funcionario {
+	
+	//Atributos
+	private String nome;
+	private double salario;
+	private Endereco endereco;
+	
+	//Getters e Setters
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public double getSalario() {
+		return salario;
+	}
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
+	public Endereco getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
+	
+	
+	
+	
+
+}
