b8238173f0befed6df5faa87be222c9acf7ee184
Crillin222 <iuricasagrande@gmail.com>
2022-11-03 20:31:54 -0300

    adicionado metodos no exemplo de excecoes

--- excecoes_exemplo/src/application/Program.java
+++ excecoes_exemplo/src/application/Program.java
@@ -6,26 +6,42 @@ import java.util.Scanner;
 public class Program {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
 		
-		try {
-		String[] vect = sc.nextLine().split(" ");
-		int position = sc.nextInt();
-		System.out.println(vect[position]);
-		}
-		//IF THE POSITION ENTERED DIDNT EXIST
-		catch (ArrayIndexOutOfBoundsException e) {
-			System.out.println("Invalid Position");
-		}
-		//IF THE POSITION ENTERED ISNT A NUMBER
-		catch (InputMismatchException e) {
-			System.out.println("Input error");
-		}
+		method1();
 		
 		System.out.println("End of program");
 		
-		sc.close();
 
 	}
+	
+	public static void method1() {
+		System.out.println("****METHOD1 START****");
+		method2();
+		System.out.println("****METHOD1 END****");
+	}
 
+	public static void method2() {
+		System.out.println("****METHOD2 START****");
+		Scanner sc = new Scanner(System.in);
+		
+		try {
+			String[] vect = sc.nextLine().split(" ");
+			int position = sc.nextInt();
+			System.out.println(vect[position]);
+			}
+			//IF THE POSITION ENTERED DIDNT EXIST
+			catch (ArrayIndexOutOfBoundsException e) {
+				System.out.println("Invalid Position");
+				e.printStackTrace();
+				sc.next();
+			}
+			//IF THE POSITION ENTERED ISNT A aNUMBER
+			catch (InputMismatchException e) {
+				System.out.println("Input error");
+			}
+		
+		sc.close();
+		System.out.println("****METHOD2 END****");
+	}
+	
 }
